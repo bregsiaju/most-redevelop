8f59c0e33a2e05a754c9158b142b83be51cfb263
Bregsi Atingsari Julastri <bregsi.atingsari@suitmedia.com>
2023-03-21 12:03:34 +0700

    fix: header responsive (-) dropdown menu

--- components/header/MainHeader.vue
+++ components/header/MainHeader.vue
@@ -36,7 +36,7 @@
 		<div class="main-header bg-white">
 			<div class="container flex f-space-between v-center">
 				<Logo />
-				<div class="nav-collapse flex v-center">
+				<div class="flex v-center" :class="{ 'nav-collapse': isCollapse }">
 					<ul class="list-nostyle flex">
 						<li>
 							<nuxt-link to="">Produk <i class="bzi-angle-down"></i></nuxt-link>
@@ -73,7 +73,18 @@
 						alt="Logo Mandiri"
 					/>
 				</div>
+				<div v-if="isCollapse" class="menu-responsive flex v-center">
+					<div class="search relative">
+						<i class="icon-search" @click="searchOn = true"></i>
+					</div>
+					<div class="menu" @click="showSidebar = !showSidebar">
+						<i :class="showSidebar ? 'bzi-close' : 'bzi-bars'"></i>
+					</div>
+				</div>
 			</div>
+		</div>
+		<div v-if="showSidebar">
+			<headerSidebar />
 		</div>
 	</header>
 </template>
@@ -83,18 +94,28 @@ export default {
 	data() {
 		return {
 			isSticky: false,
-			searchOn: false
+			searchOn: false,
+			isCollapse: false,
+			showSidebar: false
 		}
 	},
 	mounted() {
 		window.addEventListener('scroll', this.handleScroll)
+		window.addEventListener('resize', this.handleResize)
+		this.handleResize()
 	},
 	beforeDestroy() {
 		window.removeEventListener('scroll', this.handleScroll)
+		window.removeEventListener('resize', this.handleResize)
 	},
 	methods: {
 		handleScroll() {
 			this.isSticky = window.pageYOffset > this.$el.offsetTop
+		},
+		handleResize() {
+			window.innerWidth <= 770
+				? (this.isCollapse = true)
+				: (this.isCollapse = false)
 		}
 	}
 }
@@ -109,8 +130,12 @@ export default {
 	transition: all 0.5s ease;
 
 	&.sticky {
-		transform: translateY(-46px);
+		transform: none;
 		box-shadow: 0 0px 7px rgba(0, 0, 0, 0.1);
+
+		@media #{$large} {
+			transform: translateY(-46px);
+		}
 	}
 }
 .mini-header {
@@ -139,6 +164,7 @@ export default {
 
 .main-header {
 	border-bottom: 1px solid #e6e6e6;
+	position: relative;
 
 	.container {
 		padding: 8px 0;
@@ -165,10 +191,20 @@ export default {
 	}
 }
 
-@media (max-width: 768px) {
+@media (max-width: 770px) {
 	.nav-collapse,
 	.mini-header {
 		display: none;
+	}
+}
+
+.menu-responsive {
+	i {
+		font-size: 20px;
+
+		&:hover {
+			cursor: pointer;
+		}
 	}
 }
 </style>
---
+++ components/header/Sidebar.vue
@@ -0,0 +1,90 @@
+<template>
+	<div class="sidebar">
+		<ul class="list-nostyle">
+			<li><nuxt-link to="">Produk</nuxt-link><i class="bzi-angle-down"></i></li>
+			<li>
+				<nuxt-link to="">Aplikasi</nuxt-link><i class="bzi-angle-down"></i>
+			</li>
+			<li>
+				<nuxt-link to="">Belajar</nuxt-link><i class="bzi-angle-down"></i>
+			</li>
+			<li>
+				<nuxt-link to="">Riset & Berita</nuxt-link>
+				<i class="bzi-angle-down"></i>
+			</li>
+			<li><nuxt-link to="">Forum</nuxt-link><i class="bzi-angle-down"></i></li>
+		</ul>
+		<div class="btn-login">
+			<nuxt-link to="/login" class="btn--primary">MULAI INVESTASI</nuxt-link>
+		</div>
+		<hr />
+		<div class="helper">
+			<nuxt-link to="/cara-daftar">Cara Daftar</nuxt-link>
+			<hr />
+			<a href="tell:14032" target="_blank" class="flex v-center f-gap-8 mb-8">
+				<i class="icon-phone"></i>
+				<span>Care Center <b>14032</b></span>
+			</a>
+			<nuxt-link to="/bantuan">Bantuan</nuxt-link>
+		</div>
+		<hr />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Sidebar'
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+	background: white;
+	width: 100%;
+	height: 100vh;
+	position: absolute;
+	z-index: 5;
+	overflow: auto;
+	padding: 12px 16px;
+
+	ul > li {
+		padding: 12px 0;
+		text-transform: uppercase;
+		font-family: 'Open Sans';
+		color: #5f5f5f;
+		font-weight: 600;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+
+		a {
+			flex-grow: 1;
+		}
+
+		i {
+			padding: 0 8px;
+
+			&:hover {
+				cursor: pointer;
+			}
+		}
+	}
+}
+
+.btn-login {
+	margin-top: 16px;
+	padding-bottom: 12px;
+
+	.btn--primary {
+		width: 100%;
+	}
+}
+
+hr {
+	margin: 16px 0;
+}
+
+.helper {
+	color: #5f5f5f;
+}
+</style>
