3dc330f4d831fb94b1c290bab330527c2aad1268
Bregsi Atingsari Julastri <bregsi.atingsari@suitmedia.com>
2023-03-27 10:10:53 +0700

    feat: create an error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,18 +1,16 @@
 <template>
-	<div class="container text-center">
-		<div v-if="statusCode === 404" class="mb-24">
-			<img v-lazy-load data-src="~assets/img/404.png" alt="" />
-			<p class="h2 text-primary">
-				Maaf, halaman yang kamu cari untuk saat ini sedang tidak tersedia.
-			</p>
+	<div class="error sticky-footer-container-item --pushed">
+		<div class="container text-center">
+			<div v-if="statusCode === 404" class="mb-24">
+				<img v-lazy-load data-src="~assets/img/404.png" alt="" />
+				<p class="h2">Maaf, halaman yang sedang Anda cari tidak tersedia.</p>
+			</div>
+			<div v-else class="mb-24">
+				<img v-lazy-load data-src="~assets/img/500.png" alt="" />
+				<p class="h2">Maaf, saat ini sedang terjadi kesalahan basis data.</p>
+			</div>
+			<a class="btn--primary" href="/">KEMBALI KE HALAMAN AWAL</a>
 		</div>
-		<div v-else class="mb-24">
-			<img v-lazy-load data-src="~assets/img/500.png" alt="" />
-			<p class="h2 text-primary">
-				Maaf, saat ini sedang terjadi kesalahan basis data.
-			</p>
-		</div>
-		<a class="btn--primary" href="/">Return to Home</a>
 	</div>
 </template>
 
@@ -42,6 +40,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.error {
+	background-color: $primary-blue;
+	color: white;
+	width: 100%;
+}
 .container {
 	padding: 60px 0;
 	max-width: 590px !important;
