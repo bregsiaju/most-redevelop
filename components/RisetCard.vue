5bf8ee6aa4026cd99ba888f75391c03ab2fb36d5
Bregsi Atingsari Julastri <bregsi.atingsari@suitmedia.com>
2023-03-30 11:59:08 +0700

    add riset's page

---
+++ components/RisetCard.vue
@@ -0,0 +1,95 @@
+<template>
+	<div class="card relative">
+		<div class="card--content">
+			<div>
+				<p class="f-18 text-primary-blue mb-12">{{ content.date }}</p>
+				<p class="f-18 text-dark-grey">{{ content.topic }}</p>
+				<p class="card--title">{{ content.title }}</p>
+			</div>
+			<span class="f-18"
+				>Dilihat <strong>{{ content.totalViews }}</strong></span
+			>
+		</div>
+		<div
+			class="button-hover"
+			@mouseover="cardHovered = true"
+			@mouseleave="cardHovered = false"
+		>
+			<i class="bzi-plus"></i>
+		</div>
+		<div
+			v-show="cardHovered"
+			class="hover--content"
+			@mouseover="cardHovered = true"
+			@mouseleave="cardHovered = false"
+		>
+			<p class="f-20 text-semi">Emiten Pilihan</p>
+			<button class="btn--ghost-light-grey btn--block mb-16">BACA</button>
+			<button class="btn--primary btn--block">UNDUH</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'RisetCard',
+	props: {
+		content: {
+			type: Object,
+			required: true
+		}
+	},
+	data() {
+		return {
+			cardHovered: false
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+	background: #f2f2f2;
+	padding: 16px 27px 7px 16px;
+	height: 100%;
+
+	.card--content {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+
+	.card--title {
+		font-size: 30px;
+		font-weight: 600;
+		line-height: 1.3;
+	}
+
+	.button-hover {
+		position: absolute;
+		bottom: 0px;
+		right: 0px;
+		background-color: #e2e2e2;
+		color: $primary-blue;
+		padding: 14px 11px 7px 20px;
+		border-radius: 80% 0 0 0;
+	}
+
+	.hover--content {
+		position: absolute;
+		height: 100%;
+		width: 100%;
+		top: 0;
+		left: 0;
+		background-color: rgba(0, 0, 0, 0.8);
+		color: white;
+		padding: 25px 36px;
+
+		p {
+			border-bottom: 1px solid white;
+			width: fit-content;
+		}
+	}
+}
+</style>
---
+++ pages/riset.vue
@@ -0,0 +1,203 @@
+<template>
+	<main class="site-main">
+		<div class="container pt-28">
+			<ul class="breadcrumb">
+				<li><nuxt-link to="/riset-berita">Terbaru</nuxt-link></li>
+				<li>Riset & Stockpick</li>
+			</ul>
+			<!-- <p class="title">Rekomendasi <strong>Saham</strong> Hari Ini</p> -->
+			<p class="title"><strong>Riset</strong> Harian</p>
+			<p class="text-grey f-24">
+				Lihat hasil-hasil analisa harian seputar saham di sini.
+			</p>
+			<div class="filters">
+				<li>Filters</li>
+				<div class="bzg">
+					<div class="bzg_c" data-col="s4">
+						<span>
+							<input
+								type="text"
+								class="form-input form-input--block"
+								placeholder="Dari Tanggal"
+							/>
+						</span>
+						<i class="bzi-plus"></i>
+					</div>
+					<div class="bzg_c" data-col="s4">
+						<span>
+							<input
+								type="text"
+								class="form-input form-input--block"
+								placeholder="Sampai Tanggal"
+							/>
+						</span>
+						<i class="bzi-plus"></i>
+					</div>
+					<div class="bzg_c" data-col="s4">
+						<span>Jenis Riset</span>
+						<i class="bzi-plus"></i>
+					</div>
+				</div>
+			</div>
+
+			<!-- riset's result -->
+			<div class="bzg result">
+				<div
+					v-for="(result, i) in results"
+					:key="i"
+					class="bzg_c"
+					data-col="m6, l4"
+				>
+					<RisetCard :content="result" />
+				</div>
+			</div>
+			<!-- end result -->
+
+			<div class="btn-reload">LAINNYA</div>
+		</div>
+	</main>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			results: [
+				{
+					date: '08 Agu 2020',
+					topic: 'Technical Analysis',
+					title: 'Technical Analysis untuk 8 Juli 2020',
+					totalViews: 102
+				},
+				{
+					date: '08 Jul 2020',
+					topic: 'Matrix Evaluation',
+					title: 'Matriks Valuasi Saham 06 Juli 2020',
+					totalViews: 110
+				},
+				{
+					date: '08 Jun 2020',
+					topic: 'Investor Digest',
+					title: 'Investor Digest 30 Mei 2020',
+					totalViews: 200
+				},
+				{
+					date: '08 Jul 2020',
+					topic: 'Matrix Evaluation',
+					title: 'Matriks Valuasi Saham 06 Juli 2020',
+					totalViews: 110
+				},
+				{
+					date: '08 Agu 2020',
+					topic: 'Technical Analysis',
+					title: 'Technical Analysis untuk 8 Juli 2020',
+					totalViews: 102
+				},
+				{
+					date: '08 Jun 2020',
+					topic: 'Investor Digest',
+					title: 'Investor Digest 30 Mei 2020',
+					totalViews: 200
+				},
+				{
+					date: '08 Jun 2020',
+					topic: 'Investor Digest',
+					title: 'Investor Digest 30 Mei 2020',
+					totalViews: 200
+				},
+				{
+					date: '08 Agu 2020',
+					topic: 'Technical Analysis',
+					title: 'Technical Analysis untuk 8 Juli 2020',
+					totalViews: 102
+				},
+				{
+					date: '08 Jul 2020',
+					topic: 'Matrix Evaluation',
+					title: 'Matriks Valuasi Saham 06 Juli 2020',
+					totalViews: 110
+				}
+			]
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+	font-size: 45px;
+	line-height: 1;
+	color: $dark-grey;
+	margin-bottom: 25px;
+}
+
+.filters {
+	padding-bottom: 50px;
+
+	@media #{$large} {
+		width: 40%;
+	}
+
+	.bzg {
+		border-radius: 3px;
+		background-color: $primary-blue;
+		margin-left: 0;
+		color: white;
+		font-size: 16px;
+		margin-top: 16px;
+
+		.bzg_c {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			padding: 15px;
+
+			.bzi-plus:before {
+				font-size: 10px;
+			}
+		}
+
+		.bzg_c:not(:last-child) {
+			border-right: 1px solid #d2d2d2;
+		}
+
+		.form-input {
+			background: transparent;
+			border: none;
+			color: white;
+			padding: 0;
+
+			&::placeholder {
+				color: white;
+				opacity: 1;
+			}
+		}
+	}
+}
+
+.bzg.result {
+	margin-left: -30px;
+
+	.bzg_c {
+		padding-left: 30px;
+		margin-bottom: 30px;
+	}
+}
+
+.btn-reload {
+	margin: 70px auto;
+	width: 100px;
+	height: 100px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: $primary-blue;
+	border-radius: 50%;
+	border: 1px solid $primary-blue;
+
+	&:hover {
+		cursor: pointer;
+		box-shadow: inset 0 0 0 3px #e6e6e6;
+	}
+}
+</style>
