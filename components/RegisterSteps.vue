34855e109bcc46fbe2d7cb6589226cb248fc5045
Bregsi Atingsari Julastri <bregsi.atingsari@suitmedia.com>
2023-04-12 16:10:03 +0700

    finishing page of registration steps

---
+++ components/RegisterSteps.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="bzg">
+		<div v-for="(step, i) in data" :key="i" class="bzg_c mb-24" data-col="m4">
+			<div class="card-tutorial">
+				<p class="f-24 f--semi">{{ step.text1 }}</p>
+				<div class="responsive-media r-square">
+					<img
+						:src="`/assets/img/help/${step.image}`"
+						width="85px"
+						class="--contain"
+					/>
+				</div>
+				<p class="mt-24 mb-0" v-html="step.text2"></p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'RegisterSteps',
+	props: {
+		data: {
+			type: Array,
+			required: true
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.bzg {
+	margin-left: -30px;
+
+	.bzg_c {
+		padding-left: 30px;
+	}
+}
+
+.card-tutorial {
+	background-color: #fff;
+	box-shadow: 0px 6px 30px #00000015;
+	border-radius: 8px;
+	padding: 36px 30px;
+	text-align: center;
+	height: 100%;
+
+	/deep/ a {
+		color: $primary-blue;
+		text-decoration: underline;
+	}
+
+	.responsive-media {
+		max-width: 80px;
+		height: 80px;
+		margin: 0 auto;
+
+		&.r-square {
+			padding-bottom: 30%;
+		}
+	}
+}
+</style>
--- pages/cara-daftar.vue
+++ pages/cara-daftar.vue
@@ -24,26 +24,39 @@
 		<section class="bg-grey">
 			<div class="container">
 				<p class="f-24">Buka Rekening <b>Online</b></p>
-				<h1 class="title">
+				<h1 class="title mb-48">
 					Bergabung via online yang<br />pastinya <u>bebas ribet!</u>
+					<nuxt-link to="">
+						<i class="icon-arrow-right"></i>
+					</nuxt-link>
 				</h1>
-				<div class="bzg">
-					<div class="bzg_c" data-col="m4">
-						<div class="card-tutorial">
-							<p class="f-24 f--semi">Foto e-KTP dan swafoto bersama e-KTP</p>
-							<img src="/assets/img/help/icon-photo.png" width="85px" />
-							<p class="mt-24">
-								Pastikan foto e-KTP serta swafoto yang Anda kirimkan jelas dan
-								tidak terpotong
-							</p>
-						</div>
-					</div>
-					<div class="bzg_c" data-col="m4">
-						<div class="card-tutorial">s</div>
-					</div>
-					<div class="bzg_c" data-col="m4">
-						<div class="card-tutorial">s</div>
-					</div>
+				<RegisterSteps :data="onlineSteps" />
+			</div>
+		</section>
+		<section>
+			<div class="container">
+				<p class="f-24">Buka Rekening <b>Offline</b></p>
+				<h1 class="title mb-48">
+					Biarkan kami memandu<br />Anda untuk <u>bergabung.</u>
+				</h1>
+				<RegisterSteps :data="offlineSteps" />
+			</div>
+		</section>
+		<section class="help pb-0">
+			<div class="bzg bzg--no-gutter">
+				<div class="bzg_c" data-col="m6">
+					<h1 class="f-40">Lokasi Cabang</h1>
+					<p class="f-28">
+						Lihat informasi terkait lokasi cabang terdekat Anda
+					</p>
+					<nuxt-link to="/tentang-kami" class="btn--primary"
+						>SELENGKAPNYA</nuxt-link
+					>
+				</div>
+				<div class="bzg_c" data-col="m6">
+					<h1 class="f-40">Bantuan</h1>
+					<p class="f-28">Lihat seluruh informasi yang dapat membantu Anda</p>
+					<nuxt-link to="/bantuan" class="btn--primary">SELENGKAPNYA</nuxt-link>
 				</div>
 			</div>
 		</section>
@@ -51,7 +64,53 @@
 </template>
 
 <script>
-export default {}
+import RegisterSteps from '~/components/RegisterSteps.vue'
+export default {
+	component: {
+		RegisterSteps
+	},
+	data() {
+		return {
+			onlineSteps: [
+				{
+					text1: 'Foto e-KTP dan swafoto bersama e-KTP',
+					image: 'icon-photo.png',
+					text2:
+						'Pastikan foto e-KTP serta swafoto yang Anda kirimkan jelas dan tidak terpotong'
+				},
+				{
+					text1: 'Siapkan NPWP Anda kemudian foto',
+					image: 'icon-npwp.png',
+					text2: 'Unggah foto NPWP Anda dan pastikan gambar terlihat jelas'
+				},
+				{
+					text1: 'Informasi Rekening Tabungan Pribadi di Bank Mandiri',
+					image: 'icon-rekening.png',
+					text2:
+						'Persiapkan data yang diperlukan mengenai tabungan rekening pribadi Bank Mandiri'
+				}
+			],
+			offlineSteps: [
+				{
+					text1: 'Unduh Formulir Pembukaan Rekening Efek',
+					image: 'icon-form.png',
+					text2: `Dapatkan dan isi formulir pembukaan rekening <a href="">di sini.</a>`
+				},
+				{
+					text1: 'Unduh Formulir Mandiri Tabungan Investor',
+					image: 'icon-form-investor.png',
+					text2: `Dapatkan dan isi formulir pembukaan rekening <a href="">di sini.</a>`
+				},
+				{
+					text1: 'Siapkan Berkas yang Dibutuhkan',
+					image: 'icon-photocopy.png',
+					text2:
+						'Serahkan formulir serta fotokopi KTP/Paspor, NPWP, dan Cover Buku Tabungan ke teller kami.'
+				}
+			]
+		}
+	}
+}
 </script>
 
 <style lang="scss" scoped>
@@ -59,33 +118,62 @@ section {
 	padding: 60px 0;
 
 	&.header {
-		padding: 0;
+		padding: 30px 0;
+
+		@media #{$medium} {
+			padding: 0;
+		}
 	}
 }
 
 .title {
-	font-size: 40px;
+	font-size: 30px;
 	line-height: 1.4;
 	margin-bottom: 32px;
+
+	@media #{$medium} {
+		font-size: 40px;
+	}
 }
 
 .bg-grey {
 	background-color: #f2f2f2;
 }
 
-.bzg {
-	margin-left: -30px;
-
-	.bzg_c {
-		padding-left: 30px;
-	}
+i.icon-arrow-right {
+	font-size: 16px;
+	border-radius: 50%;
+	background-color: $primary;
+	color: white;
+	padding: 8px;
+	margin-right: 16px;
 }
 
-.card-tutorial {
-	background-color: #fff;
-	box-shadow: 0px 6px 30px #00000015;
-	border-radius: 8px;
-	padding: 36px 28px;
-	text-align: center;
+section.help {
+	.bzg {
+		.bzg_c:first-child {
+			background: #f8f8f8;
+		}
+
+		.bzg_c:nth-child(2) {
+			background: #f2f2f2;
+		}
+
+		.bzg_c {
+			padding: 36px 48px;
+
+			@media #{$md} {
+				padding: 36px 32px;
+
+				.f-28 {
+					font-size: 20px !important;
+				}
+			}
+
+			@media #{$sm} {
+				padding: 36px 20px;
+			}
+		}
+	}
 }
 </style>
