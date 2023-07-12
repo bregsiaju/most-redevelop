73b999af656319cf70a436b257df4282637f19fe
bregsiaju <bregsiaju@gmail.com>
2023-07-12 19:58:55 +0700

    refactor: benefit carousel

--- components/homepage/CarouselBenefit.vue
+++ components/homepage/CarouselBenefit.vue
@@ -1,8 +1,34 @@
 <template>
 	<div class="benefit">
-		<VueSlickCarousel v-bind="settings">
+		<VueSlickCarousel ref="carouselBenefit" v-bind="settings">
 			<div v-for="benefit in benefits" :key="benefit.id" class="slick-item">
-				<homepageBenefit :content="benefit" />
+				<div class="benefit-item container">
+					<div class="bzg">
+						<div class="bzg_c relative pt-64" data-col="m6">
+							<div class="navigation">
+								<i class="icon-long-arrow-left mr-8" @click="prev"></i>
+								<i class="icon-long-arrow-right" @click="next"></i>
+							</div>
+							<span class="number" :data-content="`${benefit.id}`"></span>
+							<div class="flex f-gap-28 v-start">
+								<img
+									:src="`assets/img/homepage/${benefit.icon}`"
+									width="46px"
+								/>
+								<div class="text-wrapper">
+									<p class="mb-24">{{ benefit.heading }}</p>
+									<p class="f-24 mb-24 font-grey">{{ benefit.desc }}</p>
+									<nuxt-link to="">
+										<button class="btn--primary">LIHAT SELENGKAPNYA</button>
+									</nuxt-link>
+								</div>
+							</div>
+						</div>
+						<div class="bzg_c benefit-img" data-col="m6">
+							<img :src="`assets/img/homepage/${benefit.image}`" alt="hp" />
+						</div>
+					</div>
+				</div>
 			</div>
 		</VueSlickCarousel>
 	</div>
@@ -37,26 +63,91 @@ export default {
 				}
 			],
 			settings: {
-				arrows: true
+				arrows: false
 			}
+		}
+	},
+	methods: {
+		next() {
+			this.$refs.carouselBenefit.next()
+		},
+		prev() {
+			this.$refs.carouselBenefit.prev()
 		}
 	}
 }
 </script>
 
-<style scoped>
-.benefit >>> .slick-prev {
-	top: 130px;
-	left: 40px;
+<style lang="scss" scoped>
+@media #{$medium} {
+	.benefit-item {
+		padding: 44px 0;
+	}
 }
 
-.benefit >>> .slick-next {
-	top: 130px;
-	left: 88px;
+.navigation {
+	font-size: 32px;
+
+	i {
+		&:hover {
+			color: $primary !important;
+			cursor: pointer;
+		}
+	}
+}
+.text-wrapper {
+	max-width: 355px;
+
+	p:first-child {
+		font-size: 30px;
+		font-weight: 600;
+	}
+
+	p {
+		line-height: 1.3;
+	}
 }
 
-.benefit >>> .slick-prev:before,
-.benefit >>> .slick-next:before {
-	color: transparent !important;
+.benefit-img {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	img {
+		height: 500px;
+	}
+}
+
+.number:after {
+	content: attr(data-content);
+	position: absolute;
+	top: -10%;
+	left: 30%;
+	font-size: 350px;
+	line-height: 1;
+	font-weight: 700;
+	z-index: -1;
+	background: linear-gradient(to top, #fff 10%, #eff2f6 70%);
+	-webkit-background-clip: text;
+	background-clip: text;
+	-webkit-text-fill-color: transparent;
+}
+
+.bzg {
+	margin-left: 0;
+
+	.bzg_c {
+		padding-left: 0;
+	}
+}
+
+@media (max-width: 770px) {
+	.benefit-img {
+		padding-top: 50px;
+
+		img {
+			height: 250px;
+		}
+	}
 }
 </style>
